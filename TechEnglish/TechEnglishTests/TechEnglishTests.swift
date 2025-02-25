564340a6faf76f872ddd4501d2a73ec7c910a556
katsuyatamai <xabi.k0124football@gmail.com>
2025-02-25 07:18:37 +0100

    kickoff

---
+++ TechEnglish/TechEnglishTests/TechEnglishTests.swift
@@ -0,0 +1,17 @@
+//
+//  TechEnglishTests.swift
+//  TechEnglishTests
+//
+//  Created by KatsuyaTamai on 2025/02/25.
+//
+
+import Testing
+@testable import TechEnglish
+
+struct TechEnglishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
