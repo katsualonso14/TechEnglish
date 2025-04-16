f2a69e222e8c783b43d3a95220d3b4dd9c86a440
katsuyatamai <xabi.k0124football@gmail.com>
2025-04-16 10:42:14 +0900

    Add English Localize Settings

--- TechEnglish/TechEnglish/Model/SentenceViewController.swift
+++ TechEnglish/TechEnglish/Model/SentenceViewController.swift
@@ -16,63 +16,63 @@ class SentenceViewController: UITabBarController {
     var pronunciation = ["ɪnˈvoʊkt", "ɪkˈsɛpʃ(ə)n", "əˈkɜrd", "rɪˈleɪtɪd", "ɪɡˈzɪstɪŋ", "ɪnˈhɛrɪtɪd", "ɪmˈbɛd", "əˈpoʊzd", "fɛtʃ"]
 
     var english = [
-        NSLocalizedString("呼び出された", comment: ""),
-        NSLocalizedString("例外", comment: ""),
-        NSLocalizedString("発生する・起こる", comment: ""),
-        NSLocalizedString("関連している", comment: ""),
-        NSLocalizedString("既存", comment: ""),
-        NSLocalizedString("継承された", comment: ""),
-        NSLocalizedString("埋め込む", comment: ""),
-        NSLocalizedString("反対した", comment: ""),
-        NSLocalizedString("持ってくる", comment: "")
+        NSLocalizedString("invoked", comment: ""),
+        NSLocalizedString("exception", comment: ""),
+        NSLocalizedString("occurred", comment: ""),
+        NSLocalizedString("related", comment: ""),
+        NSLocalizedString("existing", comment: ""),
+        NSLocalizedString("inherited", comment: ""),
+        NSLocalizedString("embed", comment: ""),
+        NSLocalizedString("opposed", comment: ""),
+        NSLocalizedString("fetch", comment: "")
     ]
     //TODO: 文章やシチュエーションなども追加を検討
     
     // Intermediate Page
     var secondSentenceArray = [
-        ExpandableNames(isExpanded: true, names:  ["ancestor","modify","Contained","identifiable","Instead","verify","Permission denied","permanent"].map{
+        ExpandableNames(isExpanded: true, names:  ["ancestor","modify","Contained","identifiable","Instead","verify","Permission","permanent"].map{
             Contact(name: $0,
                 hasFavorited: false, hasFavorited2: false, hasFavorited3: false, hasFavorited4: false)
         })
     ]
 
-    var secondSentence = ["ancestor", "modify", "Contained", "identifiable", "Instead", "verify", "Permission denied", "permanent"]
+    var secondSentence = ["ancestor", "modify", "Contained", "identifiable", "Instead", "verify", "Permission", "permanent"]
 
-    var secondPronunciation = ["ˈænsɛstər", "ˈmɒdɪfaɪ", "kənˈteɪnd", "aɪˈdɛntɪˌfaɪəb(ə)l", "ɪnˈstɛd", "ˈvɛrɪfaɪ", "pɚˈmɪʃən dɪˈnaɪd", "ˈpɝːmənənt"]
+    var secondPronunciation = ["ˈænsɛstər", "ˈmɒdɪfaɪ", "kənˈteɪnd", "aɪˈdɛntɪˌfaɪəb(ə)l", "ɪnˈstɛd", "ˈvɛrɪfaɪ", "pɚˈmɪʃən", "ˈpɝːmənənt"]
 
     var secondEnglish = [
-        NSLocalizedString("祖先,先祖", comment: ""),
-        NSLocalizedString("修正,変更", comment: ""),
-        NSLocalizedString("含まれる", comment: ""),
-        NSLocalizedString("識別可能な", comment: ""),
-        NSLocalizedString("代わり", comment: ""),
-        NSLocalizedString("確認する", comment: ""),
-        NSLocalizedString("許可が降りない", comment: ""),
-        NSLocalizedString("永続", comment: "")
+        NSLocalizedString("ancestor", comment: ""),
+        NSLocalizedString("modify", comment: ""),
+        NSLocalizedString("contained", comment: ""),
+        NSLocalizedString("identifiable", comment: ""),
+        NSLocalizedString("instead", comment: ""),
+        NSLocalizedString("verify", comment: ""),
+        NSLocalizedString("permission", comment: ""),
+        NSLocalizedString("permanent", comment: "")
     ]
 
     
     // Advanced Page
     var thirdSentenceArray = [
-        ExpandableNames(isExpanded: true, names:  ["indices","constraint","attempted","indeed","suspended","observable","Deprecated","absolute"].map{
+        ExpandableNames(isExpanded: true, names:  ["denied","constraint","attempted","indeed","suspended","observable","Deprecated","absolute"].map{
             Contact(name: $0,
                 hasFavorited: false, hasFavorited2: false, hasFavorited3: false, hasFavorited4: false)
         })
     ]
 
-    var thirdSentence = ["indices", "constraint", "attempted", "indeed", "suspended", "observable", "Deprecated", "absolute"]
+    var thirdSentence = ["denied", "constraint", "attempted", "indeed", "suspended", "observable", "Deprecated", "absolute"]
 
-    var thirdPronunciation = ["ˈɪn.dɪ.siːz", "kənˈstreɪnt", "əˈtɛmptɪd", "ɪnˈdiːd", "səˈspɛndɪd", "əbˈzɝː.və.bəl", "ˈdɛprɪˌkeɪtɪd", "ˈæbsəluːt"]
+    var thirdPronunciation = ["dɪˈnaɪd", "kənˈstreɪnt", "əˈtɛmptɪd", "ɪnˈdiːd", "səˈspɛndɪd", "əbˈzɝː.və.bəl", "ˈdɛprɪˌkeɪtɪd", "ˈæbsəluːt"]
 
     var thirdEnglish = [
-        NSLocalizedString("インデックス", comment: ""),
-        NSLocalizedString("制約", comment: ""),
-        NSLocalizedString("試みた", comment: ""),
-        NSLocalizedString("本当に,実際に", comment: ""),
-        NSLocalizedString("一時停止中", comment: ""),
-        NSLocalizedString("観察可能な", comment: ""),
-        NSLocalizedString("廃止された", comment: ""),
-        NSLocalizedString("絶対", comment: "")
+        NSLocalizedString("denied", comment: ""),
+        NSLocalizedString("constraint", comment: ""),
+        NSLocalizedString("attempted", comment: ""),
+        NSLocalizedString("indeed", comment: ""),
+        NSLocalizedString("suspended", comment: ""),
+        NSLocalizedString("observable", comment: ""),
+        NSLocalizedString("deprecated", comment: ""),
+        NSLocalizedString("absolute", comment: "")
     ]
 
     
